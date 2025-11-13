7c928a51ae23391362ce88dd00a1da345a750651
mattephi <contact@mattephi.com>
2025-11-13 12:54:31 +0900

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Dev shell for firefly-iii-mcp";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_20
+            bun
+          ];
+        };
+      });
+}
